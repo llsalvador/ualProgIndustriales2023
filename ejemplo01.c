bc6ecd230a5780c5a4d03b16cc1a4ed9682d69f6
llsalvador <102579806+llsalvador@users.noreply.github.com>
2023-03-14 11:11:19 +0000

    Realizado el primer ejemplo

---
+++ ejemplo01.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+char c;
+
+do
+
+{
+
+	system("cls||clear");
+
+	printf("nombre\n");
+
+	printf("=================================\n\n");
+
+
+
+
+
+	// Solucion
+
+
+
+	printf("\nDesea efectuar una nueva operacion (S/N)? ");
+
+	scanf(" %c", &c);
+
+} while ((c != 'N') && (c != 'n'));
+
+return 0;
+
+}
